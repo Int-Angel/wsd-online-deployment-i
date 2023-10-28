d412329d462dd41d051ff59671e17081922901db
Angel Padilla <56850703+Int-Angel@users.noreply.github.com>
2023-10-28 02:18:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
   return new Response(
-    "Seeking truths beyond meaning of life, you will find 43.",
+    "Hi there!",
   );
 };
 
