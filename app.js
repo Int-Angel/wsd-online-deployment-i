5381a3130e84328451370baba69fa73acd780ff8
Angel Padilla <56850703+Int-Angel@users.noreply.github.com>
2023-10-29 06:04:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-import { serve } from "./deps.js";
+import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
+import { openKv, get, set } from "https://deno.land/std/kv/mod.ts";
 
-const handleRequest = (request) => {
-  return new Response(
-    "Hi again!",
-  );
-};
+const app = new Hono();
+const kv = await openKv();
 
-serve(handleRequest, { port: 7777 });
+// Define the feedback endpoints for values 1, 2, and 3.
+for (let feedbackValue = 1; feedbackValue <= 3; feedbackValue++) {
+  // Define the GET endpoint to retrieve the count for a specific feedback value.
+  app.get(`/feedbacks/${feedbackValue}`, async (c) => {
+    const count = await get(kv, `Feedback ${feedbackValue}`);
+    return c.text(`Feedback ${feedbackValue}: ${count || 0}`);
+  });
+
+  // Define the POST endpoint to increment the count for a specific feedback value.
+  app.post(`/feedbacks/${feedbackValue}`, async (c) => {
+    const currentCount = await get(kv, `Feedback ${feedbackValue}`);
+    const newCount = (parseInt(currentCount) || 0) + 1;
+    await set(kv, `Feedback ${feedbackValue}`, newCount.toString());
+    return c.text(`Feedback ${feedbackValue} incremented to ${newCount}`);
+  });
+}
+
+export default app;
