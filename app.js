1d242098e1b29ab20e8600081cefff588e0291ce
Angel Padilla <56850703+Int-Angel@users.noreply.github.com>
2023-10-30 01:34:48 -0700

    last ttry

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-import { serve } from "./deps.js";
+import { serve } from "https://deno.land/std/http/server.ts"; // Updated import for serve
 import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
 
 const app = new Hono();
+const server = serve({ port: 7777 }); // Create a server instance
 
 async function getFeedback(key) {
-    const kv = await Deno.openKv();
-    const count = await kv.get([key]);
-    return count.value ? parseInt(count.value) : 0;
+  const kv = await Deno.openKv();
+  const count = await kv.get(key); // Change to kv.get(key)
+  return count ? parseInt(count) : 0; // Use count directly
 }
 
 async function incrementFeedback(key) {
-    const kv = await Deno.openKv();
-    const currentCount = await getFeedback(key);
-    await kv.set([key], (currentCount + 1));
+  const kv = await Deno.openKv();
+  const currentCount = await getFeedback(key); // Fix the function name
+  await kv.set(key, (currentCount + 1).toString()); // Use key and stringify the value
 }
 
 app.get("/feedbacks/1", async (c) => {
-  const kv = await Deno.openKv();
-  await getFeedback("1");
+  const count = await getFeedback("1"); // Pass the key as a string
   return c.text(`Feedback 1: ${count}`);
 });
 
 app.post("/feedbacks/1", async (c) => {
-    await incrementFeedback("1");
-    return c.text("Feedback 1 incremented");
+  await incrementFeedback("1");
+  return c.text("Feedback 1 incremented");
 });
 
 app.get("/feedbacks/2", async (c) => {
-  const kv = await Deno.openKv();
-  await getFeedback("2");
+  const count = await getFeedback("2");
   return c.text(`Feedback 2: ${count}`);
 });
 
 app.post("/feedbacks/2", async (c) => {
   await incrementFeedback("2");
-    return c.text("Feedback 2 incremented");
+  return c.text("Feedback 2 incremented");
 });
 
 app.get("/feedbacks/3", async (c) => {
-  const kv = await Deno.openKv();
-  await getFeedback("3");
+  const count = await getFeedback("3");
   return c.text(`Feedback 3: ${count}`);
 });
 
 app.post("/feedbacks/3", async (c) => {
   await incrementFeedback("3");
-    return c.text("Feedback 3 incremented");
+  return c.text("Feedback 3 incremented");
 });
 
-serve(app.fetch, { port: 7777 });
+for await (const req of server) {
+  app.handleRequest(req);
+}
