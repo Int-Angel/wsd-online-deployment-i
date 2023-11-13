223460387f25e9292da3cb280e86aa4365ef759c
Int-Angel <padilla.angel88@gmail.com>
2023-11-13 17:22:34 +0200

    sessions feedback

--- coursesController.js
+++ coursesController.js
@@ -1,8 +1,28 @@
 import { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
 import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 import * as coursesService from "./coursesService.js";
+import {
+  getSignedCookie,
+  setSignedCookie,
+} from "https://deno.land/x/hono@v3.7.4/helper.ts";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+const sessionFeedback = new Set();
+const secret = "secret";
+
+const generateKey = (sessionId, courseId) => {
+  return `${sessionId}-${courseId}`;
+};
+
+const submitFeedbackCourse = (sessionId, courseId) => {
+  const key = generateKey(sessionId, courseId);
+  sessionFeedback.add(key);
+};
+
+const canSessionSubmitFeedback = (sessionId, courseId) => {
+  const key = generateKey(sessionId, courseId);
+  return !sessionFeedback.has(key);
+};
 
 const validator = z.object({
   course: z.string().min(4, {
@@ -38,11 +58,19 @@ const createCourse = async (c) => {
 
 const showCourse = async (c) => {
   const id = c.req.param("courseId");
+
+  const sessionId =
+    (await getSignedCookie(c, secret, "sessionId")) ?? crypto.randomUUID();
+
+  if (!canSessionSubmitFeedback(sessionId, id)) {
+    return c.text(
+      "You have already given feedback for this course. Thank you!",
+    );
+  }
+
   return c.html(
     eta.render("course.eta", { course: await coursesService.getCourse(id) }),
   );
-  //const course = await coursesService.getCourse(id);
-  //return c.text(course.course);
 };
 
 const updateCourse = async (c) => {
@@ -70,6 +98,18 @@ const incrementFeedback = async (c) => {
   const key = c.req.param("id");
   const courseId = c.req.param("courseId");
 
+  const sessionId =
+    (await getSignedCookie(c, secret, "sessionId")) ?? crypto.randomUUID();
+
+  await setSignedCookie(c, "sessionId", sessionId, secret, {
+    path: `/courses/${courseId}`,
+  });
+
+  if (!canSessionSubmitFeedback(sessionId, courseId)) {
+    return c.redirect(`/courses/${courseId}`);
+  }
+
+  submitFeedbackCourse(sessionId, courseId);
   await coursesService.incrementFeedback(key, courseId);
   return c.redirect(`/courses/${courseId}`);
 };
